567621e4384a35921922f364652d8730302e936e
Sathishraaj <123050378+Sathishraaj@users.noreply.github.com>
2023-04-12 11:32:34 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+function done(){
+    let ab =document.getElementById("First").value;
+    let abb =document.getElementById("Middle").value;
+    let abbb =document.getElementById("last").value;
+    let abbbb =document.getElementById("Email").value;
+    let abc =document.getElementById("Address").value;
+
+    console.log(ab);
+    console.log(abb);
+    console.log(abbb);
+    console.log(abbbb);
+    console.log(abc);
+
+}
+
+
